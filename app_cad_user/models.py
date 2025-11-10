9ce95037d16f4ce7405442847207bfe351458779
brenocmlo <brenocamelo@edu.unifor.br>
2025-11-10 16:18:07 -0300

    Etapas finais

--- app_cad_user/models.py
+++ app_cad_user/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    nome =  models.TextField(max_length=255)
+    idade = models.IntegerField()
--- app_cad_user/views.py
+++ app_cad_user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicial(request):
+        return render(request, 'usuarios/inicial.html')
+    def usuario(request):
+        novo_user = Usuario()
+        novo_user.nome = request.POST.get('nome')
+        novo_user.idade = request.POST.get('idade')
+        novo_user.save()
+        usuarios = {
+            'usuarios': Usuario.objects.all()
+        }
+        return render(request, 'usuarios/listagem.html', usuarios)
