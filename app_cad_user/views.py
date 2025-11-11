de5971781773e0177a92541abac4d8e7a0353b7d
brenocmlo <brenocamelo@edu.unifor.br>
2025-11-11 19:44:42 -0300

    Commits finais pt1

--- app_cad_user/views.py
+++ app_cad_user/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Usuario  
 
 def inicial(request):
-        return render(request, 'usuarios/inicial.html')
-    def usuario(request):
+    return render(request, 'usuarios/inicial.html')
+
+def usuario(request):
+    if request.method == 'POST':
         novo_user = Usuario()
         novo_user.nome = request.POST.get('nome')
         novo_user.idade = request.POST.get('idade')
         novo_user.save()
-        usuarios = {
-            'usuarios': Usuario.objects.all()
-        }
-        return render(request, 'usuarios/listagem.html', usuarios)
+        return redirect('listagem_user')  
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    return render(request, 'usuarios/listagem.html', usuarios)
